e293a90cf76800f867f6e99627c46c266999880b
1data <1data@1datadeMacBook-Pro.local>
2018-10-14 14:44:32 +0800

    忽略robot协议

--- tutorial/settings.py
+++ tutorial/settings.py
@@ -19,7 +19,7 @@ NEWSPIDER_MODULE = 'tutorial.spiders'
 #USER_AGENT = 'tutorial (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
@@ -88,3 +88,16 @@ ROBOTSTXT_OBEY = True
 #HTTPCACHE_DIR = 'httpcache'
 #HTTPCACHE_IGNORE_HTTP_CODES = []
 #HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
+
+
+'''
+SCHEDULER = "scrapy_redis.scheduler.Scheduler"
+DUPEFILTER_CLASS = "scrapy_redis.dupefilter.RFPDupeFilter"
+ITEM_PIPELINES = {
+    'scrapy_redis.pipelines.RedisPipeline': 300
+}
+'''
+
+
+
+
--- tutorial/spiders/edata.py
+++ tutorial/spiders/edata.py
@@ -3,7 +3,19 @@ import scrapy
 from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
 
+from scrapy_redis.spiders import RedisSpider
 
+
+class EdataSpider(RedisSpider):
+    name = 'edata'
+    redis_key = 'edata:start_urls'
+
+    def parse(self, response):
+        # do stuff
+        pass
+
+
+'''
 class EdataSpider(CrawlSpider):
     name = 'edata'
     allowed_domains = ['tech.china.com']
@@ -13,9 +25,24 @@ class EdataSpider(CrawlSpider):
         Rule(LinkExtractor(allow=r'Items/'), callback='parse_item', follow=True),
     )
 
+    def __init__(self, *args, **kwargs):
+        print("iiiiiiiiiiiiiiiiiiiiiiiiii")
+        super(EdataSpider, self).__init__(*args, **kwargs)
+        pass
+
+    def _make_request(self, mframe, hframe, body):
+        print("mmmmmmmmmmmmmmmmmmmmmmmmmm")
+        url = body.decode()
+        return scrapy.Request(url, callback=self.parse)
+
     def parse_item(self, response):
         i = {}
         #i['domain_id'] = response.xpath('//input[@id="sid"]/@value').extract()
         #i['name'] = response.xpath('//div[@id="name"]').extract()
         #i['description'] = response.xpath('//div[@id="description"]').extract()
         return i
+'''
+
+
+
+
