69c26026b1f774e6077215358e22716a24b90259
1data <1data@1datadeMacBook-Pro.local>
2018-11-13 15:06:25 +0800

    支持闭包、虚类序列化

---
+++ tutorial/framework/apscheduler/jsonsqlalchemy.py
@@ -0,0 +1,343 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import
+
+from apscheduler.jobstores.base import BaseJobStore, JobLookupError, ConflictingIdError
+from apscheduler.util import maybe_ref, datetime_to_utc_timestamp, utc_timestamp_to_datetime
+from apscheduler.job import Job
+
+import warnings
+
+try:
+#    import cPickle as pickle
+    from cPickle import HIGHEST_PROTOCOL
+except ImportError:  # pragma: nocover
+#    import pickle
+    from pickle import HIGHEST_PROTOCOL
+    
+import cloudpickle as pickle
+    
+import json
+
+try:
+    from sqlalchemy import (
+        create_engine, Table, Column, MetaData, Unicode, Float, LargeBinary, String, select)
+    from sqlalchemy.exc import IntegrityError
+    from sqlalchemy.sql.expression import null
+except ImportError:  # pragma: nocover
+    raise ImportError('SQLAlchemyJobStore requires SQLAlchemy installed')
+
+from apscheduler.jobstores.sqlalchemy import SQLAlchemyJobStore
+
+
+# 使用json序列化
+class JsonSQLAlchemyJobStore(SQLAlchemyJobStore):
+    def __init__(self, url=None, engine=None, tablename='apscheduler_jobs', metadata=None,
+                 pickle_protocol=HIGHEST_PROTOCOL, tableschema=None, engine_options=None):
+        super(JsonSQLAlchemyJobStore, self).__init__(url, engine, tablename, metadata,
+                 pickle_protocol, tableschema, engine_options)
+        self.pickle_protocol = pickle_protocol
+        metadata = maybe_ref(metadata) or MetaData()
+
+        if engine:
+            self.engine = maybe_ref(engine)
+        elif url:
+            self.engine = create_engine(url, **(engine_options or {}))
+        else:
+            raise ValueError('Need either "engine" or "url" defined')
+
+        # 191 = max key length in MySQL for InnoDB/utf8mb4 tables,
+        # 25 = precision that translates to an 8-byte float
+        self.jobs_t = Table(
+            tablename, metadata,
+            Column('id', Unicode(191, _warn_on_bytestring=False), primary_key=True),
+            Column('next_run_time', Float(25), index=True),
+            Column('job_state', LargeBinary, nullable=False),
+            Column('job_json', String(512), nullable=False),
+            schema=tableschema
+        )
+        
+#    def start(self, scheduler, alias):
+#        super(JsonSQLAlchemyJobStore, self).start(scheduler, alias)
+#        self.jobs_t.create(self.engine, True)
+    
+    def add_job(self, job):
+#        import cloudpickle as pickle
+#        print(job)
+#        print(pickle.dumps(job))
+#        print(job.__getstate__()['func'])
+#        exit()
+#        print(444444)
+        #print(json.dumps(job.__getstate__(), cls=JobEncoder))
+        insert = self.jobs_t.insert().values(**{
+            'id': job.id,
+            'next_run_time': datetime_to_utc_timestamp(job.next_run_time),
+            'job_state': pickle.dumps(job.__getstate__(), self.pickle_protocol),
+#            'job_json': json.dumps(job.__getstate__(), cls=JobEncoder, ensure_ascii=False)
+            'job_json': ''
+        })
+        try:
+            self.engine.execute(insert)
+        except IntegrityError:
+            raise ConflictingIdError(job.id)
+
+    def update_job(self, job):
+        update = self.jobs_t.update().values(**{
+            'next_run_time': datetime_to_utc_timestamp(job.next_run_time),
+            'job_state': pickle.dumps(job.__getstate__(), self.pickle_protocol),
+#            'job_json': json.dumps(job.__getstate__(), cls=JobEncoder, ensure_ascii=False)
+            'job_json': ''
+        }).where(self.jobs_t.c.id == job.id)
+        result = self.engine.execute(update)
+        if result.rowcount == 0:
+            raise JobLookupError(id)
+        
+    def _reconstitute_job(self, job_state, job_json):
+        job_state = pickle.loads(job_state)
+        #print("_______")
+#        print(job_state)
+#        exit()
+        #json.loads(job_json,cls=JobDecoder)
+        #print(json.loads(job_json,cls=JobDecoder))
+#        job_state = json.loads(job_json,cls=JobDecoder)
+        #print(job_json)
+        #print("--------")
+        job_state['jobstore'] = self
+        job = Job.__new__(Job)
+        job.__setstate__(job_state)
+        job._scheduler = self._scheduler
+        job._jobstore_alias = self._alias
+        return job
+    
+    def _get_jobs(self, *conditions):
+        jobs = []
+        selectable = select([self.jobs_t.c.id, self.jobs_t.c.job_state, self.jobs_t.c.job_json]).\
+            order_by(self.jobs_t.c.next_run_time)
+        selectable = selectable.where(*conditions) if conditions else selectable
+        failed_job_ids = set()
+        for row in self.engine.execute(selectable):
+            try:
+                jobs.append(self._reconstitute_job(row.job_state, row.job_json))
+            except BaseException:
+                self._logger.exception('Unable to restore job "%s" -- removing it', row.id)
+                failed_job_ids.add(row.id)
+
+        # Remove all the jobs we failed to restore
+        if failed_job_ids:
+            delete = self.jobs_t.delete().where(self.jobs_t.c.id.in_(failed_job_ids))
+            self.engine.execute(delete)
+
+        return jobs
+    
+    
+class SQLAlchemyJobStore(BaseJobStore):
+    """
+    Stores jobs in a database table using SQLAlchemy.
+    The table will be created if it doesn't exist in the database.
+    Plugin alias: ``sqlalchemy``
+    :param str url: connection string (see
+        :ref:`SQLAlchemy documentation <sqlalchemy:database_urls>` on this)
+    :param engine: an SQLAlchemy :class:`~sqlalchemy.engine.Engine` to use instead of creating a
+        new one based on ``url``
+    :param str tablename: name of the table to store jobs in
+    :param metadata: a :class:`~sqlalchemy.schema.MetaData` instance to use instead of creating a
+        new one
+    :param int pickle_protocol: pickle protocol level to use (for serialization), defaults to the
+        highest available
+    :param str tableschema: name of the (existing) schema in the target database where the table
+        should be
+    :param dict engine_options: keyword arguments to :func:`~sqlalchemy.create_engine`
+        (ignored if ``engine`` is given)
+    """
+
+    warnings.warn('SQLAlchemyJobStore has been deprecated, use JsonSQLAlchemyJobStore instead.', DeprecationWarning)
+    
+    def __init__(self, url=None, engine=None, tablename='apscheduler_jobs', metadata=None,
+                 pickle_protocol=HIGHEST_PROTOCOL, tableschema=None, engine_options=None):
+        super(SQLAlchemyJobStore, self).__init__()
+        self.pickle_protocol = pickle_protocol
+        metadata = maybe_ref(metadata) or MetaData()
+
+        if engine:
+            self.engine = maybe_ref(engine)
+        elif url:
+            self.engine = create_engine(url, **(engine_options or {}))
+        else:
+            raise ValueError('Need either "engine" or "url" defined')
+
+        # 191 = max key length in MySQL for InnoDB/utf8mb4 tables,
+        # 25 = precision that translates to an 8-byte float
+        self.jobs_t = Table(
+            tablename, metadata,
+            Column('id', Unicode(191, _warn_on_bytestring=False), primary_key=True),
+            Column('next_run_time', Float(25), index=True),
+            Column('job_state', LargeBinary, nullable=False),
+            Column('job_json', String(512), nullable=False),
+            schema=tableschema
+        )
+
+    def start(self, scheduler, alias):
+        super(SQLAlchemyJobStore, self).start(scheduler, alias)
+        self.jobs_t.create(self.engine, True)
+
+    def lookup_job(self, job_id):
+        selectable = select([self.jobs_t.c.job_state,self.jobs_t.c.job_json]).where(self.jobs_t.c.id == job_id)
+        job_state,job_json = self.engine.execute(selectable).scalar()
+        return self._reconstitute_job(job_state,job_json) if job_state else None
+
+    def get_due_jobs(self, now):
+        timestamp = datetime_to_utc_timestamp(now)
+        return self._get_jobs(self.jobs_t.c.next_run_time <= timestamp)
+
+    def get_next_run_time(self):
+        selectable = select([self.jobs_t.c.next_run_time]).\
+            where(self.jobs_t.c.next_run_time != null()).\
+            order_by(self.jobs_t.c.next_run_time).limit(1)
+        next_run_time = self.engine.execute(selectable).scalar()
+        return utc_timestamp_to_datetime(next_run_time)
+
+    def get_all_jobs(self):
+        jobs = self._get_jobs()
+        self._fix_paused_jobs_sorting(jobs)
+        return jobs
+
+    def add_job(self, job):
+        #print(json.dumps(job.__getstate__(), cls=JobEncoder))
+        insert = self.jobs_t.insert().values(**{
+            'id': job.id,
+            'next_run_time': datetime_to_utc_timestamp(job.next_run_time),
+            'job_state': pickle.dumps(job.__getstate__(), self.pickle_protocol),
+            'job_json': json.dumps(job.__getstate__(), cls=JobEncoder)
+        })
+        try:
+            self.engine.execute(insert)
+        except IntegrityError:
+            raise ConflictingIdError(job.id)
+
+    def update_job(self, job):
+        update = self.jobs_t.update().values(**{
+            'next_run_time': datetime_to_utc_timestamp(job.next_run_time),
+            'job_state': pickle.dumps(job.__getstate__(), self.pickle_protocol),
+            'job_json': json.dumps(job.__getstate__(), cls=JobEncoder)
+        }).where(self.jobs_t.c.id == job.id)
+        result = self.engine.execute(update)
+        if result.rowcount == 0:
+            raise JobLookupError(id)
+
+    def remove_job(self, job_id):
+        delete = self.jobs_t.delete().where(self.jobs_t.c.id == job_id)
+        result = self.engine.execute(delete)
+        if result.rowcount == 0:
+            raise JobLookupError(job_id)
+
+    def remove_all_jobs(self):
+        delete = self.jobs_t.delete()
+        self.engine.execute(delete)
+
+    def shutdown(self):
+        self.engine.dispose()
+
+    def _reconstitute_job(self, job_state, job_json):
+        #job_state = pickle.loads(job_state)
+        #print("_______")
+        #print(job_state)
+        #json.loads(job_json,cls=JobDecoder)
+        #print(json.loads(job_json,cls=JobDecoder))
+        job_state = json.loads(job_json,cls=JobDecoder)
+        #print(job_json)
+        #print("--------")
+        job_state['jobstore'] = self
+        job = Job.__new__(Job)
+        job.__setstate__(job_state)
+        job._scheduler = self._scheduler
+        job._jobstore_alias = self._alias
+        return job
+
+    def _get_jobs(self, *conditions):
+        jobs = []
+        selectable = select([self.jobs_t.c.id, self.jobs_t.c.job_state, self.jobs_t.c.job_json]).\
+            order_by(self.jobs_t.c.next_run_time)
+        selectable = selectable.where(*conditions) if conditions else selectable
+        failed_job_ids = set()
+        for row in self.engine.execute(selectable):
+            try:
+                jobs.append(self._reconstitute_job(row.job_state, row.job_json))
+            except BaseException:
+                self._logger.exception('Unable to restore job "%s" -- removing it', row.id)
+                failed_job_ids.add(row.id)
+
+        # Remove all the jobs we failed to restore
+        if failed_job_ids:
+            delete = self.jobs_t.delete().where(self.jobs_t.c.id.in_(failed_job_ids))
+            self.engine.execute(delete)
+
+        return jobs
+
+    def __repr__(self):
+        return '<%s (url=%s)>' % (self.__class__.__name__, self.engine.url)
+    
+    
+
+from apscheduler.triggers.cron import CronTrigger
+from apscheduler.triggers.date import DateTrigger 
+from apscheduler.triggers.interval import IntervalTrigger
+from datetime import datetime
+from datetime import tzinfo
+from pytz import timezone
+from apscheduler.triggers.cron.fields import (
+    BaseField, MonthField, WeekField, DayOfMonthField, DayOfWeekField, DEFAULT_VALUES)
+import re
+  
+class JobEncoder(json.JSONEncoder):  
+    def default(self, obj):
+        if isinstance(obj, datetime):
+            return obj.__str__() #__repr__
+        elif isinstance(obj, CronTrigger):
+#            if isinstance(obj.timezone, tzinfo):
+#                #print(type(obj.__str__()))
+#                #print(obj)
+#                obj.timezone = obj.timezone.__str__()
+#            elif isinstance(obj.timezone, timezone):
+#                #print(type(obj.__str__()))
+#                #print(obj)
+#                obj.timezone = obj.timezone.__str__()
+            return obj.__repr__()
+        return json.JSONEncoder.default(self, obj)
+    
+class JobDecoder(json.JSONDecoder): 
+    #def decode(self, s, _w=<built-in method match of re.Pattern object at 0x10609a870>)
+    def decode(self, d, **kwargs):
+        d = json.loads(d)
+        
+        #trigger
+        findall = re.findall(r"<(.*?) \((.*?,) timezone='(.*?)'\)>",d['trigger'])
+        if not findall:
+            raise "decode error!"
+        
+        name = findall[0][0]
+        #if name in ("CronTrigger", "DateTrigger", "IntervalTrigger"):
+        #    d['trigger'] = eval(name)()
+            
+        state = {}
+        #fields = {}
+        if findall[0][1]:
+            attr = re.findall(r"(.*?)='(.*?)',", findall[0][1])
+            for a in attr:
+                state[a[0]] = a[1]
+                #if a[0] not in ('start_date','end_date','fields','jitter','timezone'):
+                #    fields[a[0]] = a[1]
+                
+        #print(name)
+        if name=='CronTrigger':
+            #state['second'] = '*/6'
+            d['trigger'] = CronTrigger(**state)
+            
+        #next_run_time
+        #todo:datetime.datetime(2018, 11, 7, 17, 54, 45, tzinfo=<DstTzInfo 'Asia/Shanghai' CST+8:00:00 STD>)}
+        d['next_run_time'] = datetime.strptime(d['next_run_time'], "%Y-%m-%d %H:%M:%S%z")
+        
+        return d
+    
+        
+    
+    
--- tutorial/framework/apscheduler/mtxscheduler.py
+++ tutorial/framework/apscheduler/mtxscheduler.py
@@ -3,15 +3,59 @@
 #from apscheduler.scheduler import Scheduler
 from apscheduler.schedulers.blocking import BlockingScheduler as Scheduler
 #from apscheduler.schedulers.background import BackgroundScheduler as Scheduler
+
 import apscheduler.events
 import datetime
-import fcntl
 import socket
+import fcntl
 import struct
 import sys
-	
+from uuid import uuid4
+            
 
-def lock(opener):
+#import pickle
+import cloudpickle as pickle
+#import dill as pickle  #不能处理six.with_metaclass(ABCMeta)继承的子类
+#pickle.settings['recurse'] = True
+#import copy_reg#python2
+import copyreg
+import types
+
+#允许实例绑定方法序列化
+def _pickle_method(m):
+    if m.im_self is None:
+        return getattr, (m.im_class, m.im_func.func_name)
+    else:
+        return getattr, (m.im_self, m.im_func.func_name)
+
+#copyreg.pickle(types.MethodType, _pickle_method)
+
+def get_pickling_errors(obj,seen=None):
+    if seen == None:
+        seen = []
+    try:
+        state = obj.__getstate__()
+    except AttributeError:
+        return
+    if state == None:
+        return
+    if isinstance(state,tuple):
+        if not isinstance(state[0],dict):
+            state=state[1]
+        else:
+            state=state[0].update(state[1])
+    result = {}    
+    for i in state:
+        try:
+            pickle.dumps(state[i],protocol=2)
+        except pickle.PicklingError:
+            if not state[i] in seen:
+                seen.append(state[i])
+                result[i]=get_pickling_errors(state[i],seen)
+    return result
+
+def lock(opener, job_id=None):
+#    print(job_id)
     print('lock()')
     
     #mongo
@@ -25,7 +69,8 @@ def lock(opener):
 
     return lck
 
-def hb(ip, now, opener, **attrs):
+def hb(ip, now, opener, job_id=None, **attrs):
+#    print(job_id)
     print('heartbeat()')
     attrs['active_ip'] = ip
     attrs['update_time'] = now
@@ -36,7 +81,8 @@ def hb(ip, now, opener, **attrs):
     #print(attrs)
     #print(opener.hgetall('lockstore'))
 
-def le(lock_rec):
+def le(lock_rec, job_id=None):
+#    print(job_id)
     if lock_rec:
 #        print('active ip')
         print('active ip', lock_rec)
@@ -59,10 +105,17 @@ def get_ip(ifname):
 #                            )[20:24]
 #                        )
          
+         
+    
+#todo: scheduler阻止装饰过的脚本序列化、不能正确序列化
 class MutexScheduler(Scheduler):
     def __init__(self, gconfig={}, ** options):
         Scheduler.__init__(self, gconfig, ** options)
         self.ip = get_ip('eth0')
+        self.uuid = uuid4().hex
+        
+    def __getstate__(self):
+         return self.__dict__
         
     def start(self):
         super(MutexScheduler, self).start()
@@ -74,15 +127,20 @@ class MutexScheduler(Scheduler):
     def mutex(self, lock=None, heartbeat=None, lock_else=None, 
               unactive_interval=datetime.timedelta(seconds=30), opener=None):
 		
-        def mutex_func_gen(func):
-            def mtx_func():
-                if lock:
-                    lock_rec = lock(opener)
+        def mutex_func_gen(func, job_id):
+            #重要：不要引入外部thread\queue\等数据，降导致序列化错误
+            def mtx_func(** options):
+                opener=None #todo:xxx
+#                print(options)
+                if lock:#有锁？
+                    lock_rec = lock(opener, job_id)
                     now = datetime.datetime.now()
+#                    print(lock_rec)
+#                    print(('update_time' in lock_rec and lock_rec['update_time'] and now - lock_rec['update_time'] >= unactive_interval))
 
                     # execute mutex job when the server is active, or the other server is timeout.
-                    if not lock_rec or ('active_ip' in lock_rec and lock_rec['active_ip'] == self.ip) or ('update_time' in lock_rec and lock_rec['update_time'] and now - lock_rec['update_time'] >= unactive_interval):  
-                        #print(3333)
+                    if not lock_rec or ('active_ip' in lock_rec and lock_rec['active_ip'] == "self.ip") or ('update_time' in lock_rec and lock_rec['update_time'] and now - lock_rec['update_time'] >= unactive_interval):  
+#                        print(3333)
                         if lock_rec:
                             del lock_rec['active_ip']
                             del lock_rec['update_time']
@@ -90,50 +148,80 @@ class MutexScheduler(Scheduler):
                         if not lock_rec:
                             lock_rec = {}
 
-                        lock_attrs = func( ** lock_rec)
+#                        print(func)
+                        lock_attrs = func( ** options )#todo:执行job？
+#                        print(lock_attrs)
                         if not lock_attrs:
                             lock_attrs = {}
 
                         # send heart beat
                         #print(344444)
-                        heartbeat(self.ip, now, opener, ** lock_attrs)
+                        heartbeat("self.ip", now, opener, job_id, ** lock_attrs)#todo:返回值写入lock？
+                        return lock_attrs
                     else: 
-                        lock_else(lock_rec)
+#                        print(4444)
+                        lock_else(lock_rec, job_id)
                 else:
                     #print(1111)
-                    func()
+                    lock_attrs = func(** options)#todo:执行job？
+                    return lock_attrs
 
             return mtx_func
 
         self.mtx_func_gen = mutex_func_gen
 
         def inner(func):
+#            self.mtx_func_gen = mutex_func_gen
+#            pass
             return func
 
         return inner
 
-    def cron_schedule(self, ** options):
+    #job.id
+#    @override
+    def scheduled_job(self, ** options):
+#    def cron_schedule(self, ** options):
         def inner(func):
+#            import traceback
+#            traceback.print_stack()
+            job_id = uuid4().hex #提前生成ID，便于JOB加锁
+#            print(job_id)
             if hasattr(self, 'mtx_func_gen'):
-                func = self.mtx_func_gen(func)
+#                print(111)
+                func = self.mtx_func_gen(func, job_id)
 
-            func.job = self.add_job(func, ** options) #todo:err
+#            print(func)
+#            import pickle
+#            import cloudpickle as pickle
+#            print(func)
+##            def _pickle_method(m):
+##                print(1111111)
+##                return m, ()
+##            copyreg.pickle(func, _pickle_method)
+#            print(pickle.dumps(func))
+#            exit()
+            func.job = self.add_job(func, id=job_id, ** options)
+#            print(func)
+#            print(func.job.id)
             return func
         return inner
     
        
 # test
 if __name__=='__main__':
-#    from framework.apscheduler.sqlalchemy import JsonSQLAlchemyJobStore
-#    jobstores = {
+    from framework.apscheduler.jsonsqlalchemy import JsonSQLAlchemyJobStore
+    from apscheduler.jobstores.sqlalchemy import SQLAlchemyJobStore
+    from apscheduler.jobstores.memory import MemoryJobStore
+    jobstores = {
+    'default': MemoryJobStore(),
 ##    'mongo': MongoDBJobStore(),
 #    #'default': SQLAlchemyJobStore(url='sqlite:///jobs.sqlite'),
-#    #'mysql': SQLAlchemyJobStore(url='mysql+mysqlconnector://root:12345678@localhost/scrapy_db?charset=utf8&use_unicode=0')
+#    'mysql': SQLAlchemyJobStore(url='mysql+mysqlconnector://root:12345678@localhost/scrapy_db',tablename='apscheduler_jobs')
 #    'default': JsonSQLAlchemyJobStore(url='mysql+mysqlconnector://root:12345678@localhost/scrapy_db',tablename='apscheduler_jobs'),
-#    'mysql': JsonSQLAlchemyJobStore(url='mysql+mysqlconnector://root:12345678@localhost/scrapy_db',tablename='apscheduler_jobs')
-#    }
+    'mysql': JsonSQLAlchemyJobStore(url='mysql+mysqlconnector://root:12345678@localhost/scrapy_db',tablename='apscheduler_jobs')
+    }
 
-    sched = MutexScheduler()
+    sched = MutexScheduler(jobstores=jobstores)
     
     #import pymongo
     #mongo = pymongo.Connection(host = '127.0.0.1', port = 27017)
@@ -141,17 +229,47 @@ if __name__=='__main__':
     
     import redis
     from redis import StrictRedis as Redis
-    pool = redis.ConnectionPool(host = '127.0.0.1', port = 6379, decode_responses=True)
+    pool = redis.ConnectionPool(host = '127.0.0.1', port = 6379, db = 0, decode_responses=True)
     lock_store = Redis(connection_pool = pool)
 
     i = 0
 
+#    @sched.cron_schedule(trigger='cron', second = '*')
+    @sched.scheduled_job(trigger='cron', kwargs={"aa":'aa111',"bb":'bb'}, second = '*/1', jobstore='mysql')
     @sched.mutex(lock = lock, heartbeat = hb, lock_else = le, opener=lock_store)
-    @sched.cron_schedule(trigger='cron', second = '*')
-    def job(**attr):
+    # 由job向业务传递分片信息
+    def job(aa="",bb=""):
+        print(aa)
         global i
         i += 1
         print(i)
+       
+#    import pickle
+#    print(job)
+#    print(sched.mtx_func_gen())
+#    print(pickle.dumps(job))
+#    exit()
+#    def ff(aa="aa", bb="bb"):
+#        print(aa,bb)
+#    ff=sched.mutex(lock = lock, heartbeat = hb, lock_else = le, opener=lock_store)
+#    print(ff)
+#    exit()
+    
+#    job=sched.scheduled_job(job,trigger='cron', kwargs={"aa":'aa111',"bb":'bb'}, second = '*/1')#todo:
+#    print(job.job.id)
+    
+#    ii = 0 
+#    
+#    @sched.mutex(lock = lock, heartbeat = hb, lock_else = le, opener=lock_store)
+##    @sched.cron_schedule(trigger='cron', second = '*')
+#    @sched.scheduled_job(trigger='cron', second = '*/1')
+#    # 由job向业务传递分片信息
+#    def job2(**attr):
+#        global ii
+#        ii += 1
+#        print(ii)
+    
+#    job = sched.add_job(func=job, kwargs={"aa":"aa","bb":"bb"}, trigger='cron', second='*/1')
 
     sched.add_listener(err_listener, apscheduler.events.EVENT_JOB_ERROR)
     
